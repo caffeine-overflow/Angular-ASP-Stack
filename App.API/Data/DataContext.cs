bbae4aee73235e14ba486d2d7ebcf95e8d223c02
popcornn95 <danishdavis@hotmail.com>
2020-03-06 23:47:18 -0500

    Angular & ASP setup

---
+++ App.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using App.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
