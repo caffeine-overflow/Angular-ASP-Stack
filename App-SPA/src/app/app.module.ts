0862e322408aaa0f5d43f0f55e31764080b77eeb
popcornn95 <danishdavis@hotmail.com>
2020-03-15 15:40:35 -0400

    Third party components and styling# Please enter the commit message for your changes. Lines starting

--- App-SPA/src/app/app.module.ts
+++ App-SPA/src/app/app.module.ts
@@ -10,10 +10,11 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavbarComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule, BsDropdownModule.forRoot()],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
--- App-SPA/src/app/navbar/navbar.component.ts
+++ App-SPA/src/app/navbar/navbar.component.ts
@@ -9,7 +9,7 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavbarComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
@@ -25,8 +25,7 @@ export class NavbarComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logOut() {
