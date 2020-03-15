6f9552e92bfd8ee88401fb5adeb8702619574be6
popcornn95 <danishdavis@hotmail.com>
2020-03-14 21:03:56 -0400

    Error handling

--- App-SPA/src/app/app.module.ts
+++ App-SPA/src/app/app.module.ts
@@ -3,32 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavbarComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, ValueComponent, NavbarComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- App.API/Controllers/AuthController.cs
+++ App.API/Controllers/AuthController.cs
@@ -43,7 +43,6 @@ namespace App.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Yo");
             var userFromRepo = await _repo.Login(userForLoginDto.username.ToLower(), userForLoginDto.password);
 
             if (userFromRepo == null) return Unauthorized();
